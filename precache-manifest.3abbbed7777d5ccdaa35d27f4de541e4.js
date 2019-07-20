e59e96bc466f08fd44a86624e2ab63395d174bb0
Igor Kirnosov <xan50rus@gmail.com>
2019-07-20 20:38:15 +0300

    Updates

---
+++ precache-manifest.3abbbed7777d5ccdaa35d27f4de541e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb71e3dde3076f3cc8a728e972437ba7",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "f027c8ca448e99c47a76",
+    "url": "/react-todo-list/static/css/main.21ce9ff4.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/react-todo-list/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "f027c8ca448e99c47a76",
+    "url": "/react-todo-list/static/js/main.26e4f423.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
